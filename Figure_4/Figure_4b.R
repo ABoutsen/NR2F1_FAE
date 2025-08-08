0da12f7f95192ae8f4a057bd9295fd62e0e2da16
ABoutsen <aboutsen@uliege.be>
2025-08-08 14:07:34 +0200

    Update Figure_4b.R

--- Figure_4/Figure_4b.R
+++ Figure_4/Figure_4b.R
@@ -1,6 +1,8 @@
 library(ggplot2)
 
 cluster.markers <- readr::read_table("F:/GIGA - PHD/01_MAST_DEG_EtOH_vs_Ctrl_Migrating Neurons_filtered.tsv")
+colnames(cluster.markers) <- c("gene_name", "p_val", "avg_log2FC", "pct.1", "pct.2", "p_val_adj", "threshold")
+
 cluster.markers_10 <- cluster.markers[1:10,]
 
 cluster.markers_10$X8 = factor(ifelse(cluster.markers_10$threshold > 0, "Up", "Down"))
@@ -11,5 +13,5 @@ mid <- 0.00
 ggplot(cluster.markers_10, aes(x=X9, y=gene_name, size=-log10(p_val_adj), color=avg_log2FC)) + 
   geom_point() + 
   theme_classic() + 
-  scale_color_gradient2(midpoint = 0.00, low = "red", mid = "white", high = "blue")+
+  scale_color_gradient2(midpoint = 0.00, low = "blue", mid = "white", high = "red")+
   scale_size(range = c(1, 10))

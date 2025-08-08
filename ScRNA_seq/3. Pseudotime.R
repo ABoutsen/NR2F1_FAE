2f77a2684480682b038c2fddf49b945ea706e218
ABoutsen <aboutsen@uliege.be>
2025-08-09 01:38:30 +0200

    3. Pseudotime.R

---
+++ ScRNA_seq/3. Pseudotime.R
@@ -0,0 +1,22 @@
+library(Seurat)
+library(monocle3)
+
+seurat_OBfree <- readRDS("F:/GIGA - PHD/h5_test_OBfree.rds")
+seurat_OBfree <- subset(seurat_OBfree, ident=c("Endothelial cells", "Microglia", "Pericytes", "MGE Interneurons", "CGE Interneurons", "Cajal Retzius Cells"), invert=T)
+DimPlot(seurat_OBfree, label = T)
+
+seurat_cds <- SeuratWrappers::as.cell_data_set(seurat_OBfree)
+seurat_cds <- cluster_cells(cds = seurat_cds, reduction_method = "UMAP")
+seurat_cds <- learn_graph(seurat_cds, use_partition = T, close_loop = T)
+
+seurat_cds <- order_cells(seurat_cds, reduction_method = "UMAP")
+
+plot_cells(cds = seurat_cds)
+
+plot_cells(cds = seurat_cds,
+           color_cells_by = "pseudotime", #Subcluster #ident 
+           show_trajectory_graph = TRUE,
+           label_leaves =  F, 
+           label_branch_points = F, 
+           label_cell_groups = F, 
+) 

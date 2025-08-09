d2b5153b7830fec7a1fe359008f0c2c5f976ad12
ABoutsen <aboutsen@uliege.be>
2025-08-09 09:16:49 +0200

    Figure_4a.R

--- Figure_4/Figure_4a.R
+++ Figure_4/Figure_4a.R
@@ -3,15 +3,14 @@ library(ggplot2)
 
 seurat_OBfree <- readRDS("../../seurat_OBfree.rds")
 
-cluster.markers <- FindMarkers(seurat_OBfree, ident.2="Ctrl", ident.1="EtOH", group.by = 'type', subset.ident = "Migrating Neurons", logfc.threshold = "0", test.use = "MAST", verbose = T)
-colnames(cluster.markers) <- c("genes", "p_val", "avg_log2FC", "pct.1", "pct.2", "p_val_adj", "threshold")
+cluster.markers <- FindMarkers(seurat_OBfree, ident.2="Ctrl", ident.1="EtOH", group.by = 'type', subset.ident = "Migrating Neurons", min.pct = 0.1, logfc.threshold = "0", test.use = "MAST", verbose = T)
 
 GAD <- GetAssayData(object = seurat_OBfree, slot="counts") 
 row.names.remove.mito <- stringr::str_subset(GAD@Dimnames[[1]], "^mt-")  
 row.names.remove.hbb  <- stringr::str_subset(GAD@Dimnames[[1]], "^Hbb-") 
 row.names.remove.ribo  <- stringr::str_subset(GAD@Dimnames[[1]], "^Rps|^Rpl")
 row.names.remove.sex  <- c("Xist", "Tsix", "Uba1y", "Gm28587", "Kdm5d", "Eif2s3y","Gm29650", "Uty", "Ddx3y", "Gm21860", "Gm47283")
-cluster.markers <- cluster.markers[!(row.names(cluster.markers) %in% c(row.names.remove.sex, row.names.remove.mito, row.names.remove.hbb, row.names.remove.ribo)), ]
+cluster.markers <- cluster.markers[!(row.names(cluster.markers) %in% c(row.names.remove.sex, row.names.remove.mito, row.names.remove.ribo)), ]
 
 cluster.markers$threshold = factor(ifelse(cluster.markers$avg_log2FC > 0.1 & cluster.markers$p_val_adj < 0.01, 1, 
                                           ifelse(cluster.markers$avg_log2FC < -0.1 & cluster.markers$p_val_adj < 0.01, -1, 0)))

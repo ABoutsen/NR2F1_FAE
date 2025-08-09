287d02c82b6eda7092a36772eacb91c30fd70f59
ABoutsen <aboutsen@uliege.be>
2025-08-09 08:38:28 +0200

    Ext_figure_2d.R

--- Extended_data_figure_2/Ext_figure_2d.R
+++ Extended_data_figure_2/Ext_figure_2d.R
@@ -1,10 +1,16 @@
 library(Seurat)
 library(ggplot2)
 
-hfile <- readRDS("F:/GIGA - PHD/h5_test_OBfree.rds")
-seurat_cds<- readRDS("F:/GIGA - PHD/250514_seurat_cds.rds")
+seurat_OBfree <- readRDS("../../seurat_OBfree.rds")
 
-seurat_pseudotime <- subset(hfile, ident=c("Endothelial cells", "Microglia", "Pericytes", "CGE Interneurons", "MGE Interneurons", "Cajal Retzius Cells"), invert=T)
+# Pseudotime Analysis
+seurat_OBfree <- subset(seurat_OBfree, ident=c("Endothelial cells", "Microglia", "Pericytes", "MGE Interneurons", "CGE Interneurons", "Cajal Retzius Cells"), invert=T)
+DimPlot(seurat_OBfree, label = T)
+seurat_cds <- SeuratWrappers::as.cell_data_set(seurat_OBfree)
+seurat_cds <- cluster_cells(cds = seurat_cds, reduction_method = "UMAP")
+seurat_cds <- learn_graph(seurat_cds, use_partition = T, close_loop = T)
+
+seurat_pseudotime <- subset(seurat_OBfree, ident=c("Endothelial cells", "Microglia", "Pericytes", "CGE Interneurons", "MGE Interneurons", "Cajal Retzius Cells"), invert=T)
 seurat_pseudotime <- AddMetaData(object = seurat_pseudotime,
                                  metadata = seurat_cds@principal_graph_aux@listData$UMAP$pseudotime, 
                                  col.name = "pseudotime")
